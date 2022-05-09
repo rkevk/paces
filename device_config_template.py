ff8041bdb436564dccdba9f98d89e1ae733ab104
R. Kevin Kessing <62778296+rkevk@users.noreply.github.com>
2022-05-09 11:44:16 -0400

    Exposed unified_mem option in device_config_template.py

--- device_config_template.py
+++ device_config_template.py
@@ -14,6 +14,8 @@ whoami_device   = cupy.cuda.Device(0)       # change current device: DO NOT USE 
 vector_device   = cupy.cuda.Device(0)
 memsize_bytes   = 63*1024**3
 
+use_unified_mem = False
+
 ################################################################
 # Do not change anything in the remainder of the file:
 ################################################################
@@ -23,7 +25,7 @@ if vector_device == whoami_device:
 else:
     print("    Using two GPUs.")
 
-if True:
+if use_unified_mem:
     mempool = cupy.get_default_memory_pool()
     print("    Using default (GPU-based) memory pool.")
 else:

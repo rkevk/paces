57a1ccab23229f32f685aed430022af6342c1303
R. Kevin Kessing <r.k.kessing@gmail.com>
2022-06-25 23:42:57 +0200

    missing underscore

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ te_dict = dict(
     )
 
 prefix = "pos" if param_dict["hopping"]["J"] > 0 else "neg"
-dirname = "../adaptive_results/vib_gendatseg/%st_n%i_d%i_g%02i_coherence" % (prefix, nchain, max_HO_dim, int(paramdict["coupling"]['g']*10))
+dirname = "../adaptive_results/vib_gendatseg/%st_n%i_d%i_g%02i_coherence" % (prefix, nchain, max_HO_dim, int(param_dict["coupling"]['g']*10))
 
 ###############################
 # generate_timeline parameters:

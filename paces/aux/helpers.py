d13cd0a2b5945b46d0b7361b8b0bc2dd70d72e51
R. Kevin Kessing <r.k.kessing@gmail.com>
2026-02-26 17:30:39 +0100

    improved documentation in paces/aux/helpers.py

--- paces/aux/helpers.py
+++ paces/aux/helpers.py
@@ -23,7 +23,16 @@ def print_searchsorted_timing(verb, delta_t, size1, size2):
                 f" {delta_t*1000} ms (arg sizes {size1}, {size2}).")
 
 def obs_attrs(**kwargs):
-    """Decorator to add header and fname to observable functions"""
+    """
+    Decorator to add header and fname to observable functions.
+
+    This should be called with at least the following kwargs:
+        fname (str): Base name of the file where the corresponding observable expectation values
+            should be saved. The entire path will be derived from this fname as
+            [main_calc_dir]/observables/[fname].real and [main_calc_dir]/observables/[fname].imag.
+        header (str): The header line to be printed at the top of the file, but without the
+            preceding time and norm (i.e., just a description of the observables themselves).
+    """
     def wrapper(f):
         for attr, val in kwargs.items():
             setattr(f, attr, val)
@@ -35,7 +44,8 @@ def debug_lister(dbg_list):
     """
     Decorator to add debug description to generate_mel_xxx
 
-    debug_list should be a list of strings providing names for the debug output.
+    Args:
+        dbg_list (list): list of strings providing names for the debug output.
     """
     def wrapper(f):
         f.dbg_list = dbg_list
@@ -48,7 +58,11 @@ def flatten_dbg_dict(d):
 
 
 def cupy_unique(array):
-    """Replacement for numpy.unique with option axis=0"""
+    """
+    Find and return the unique, lexicographically sorted rows in an array.
+
+    Replacement for numpy.unique with option axis=0.
+    """
     if len(array.shape) != 2:
         raise ValueError("Input array must be 2D.")
     sortarr     = array[cupy.lexsort(array.T[::-1])]

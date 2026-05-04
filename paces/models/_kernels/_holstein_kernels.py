187a607233b19e723117ed64847e03f656162212
R. Kevin Kessing <r.k.kessing@gmail.com>
2026-05-04 14:58:57 +0200

    added 64-bit index functions to holstein kernels

--- paces/models/_kernels/_holstein_kernels.py
+++ paces/models/_kernels/_holstein_kernels.py
@@ -18,7 +18,6 @@ def add_phonon_at_exc(basis_states, posbitwidth, qhobitwidth, wordsize=32):
         qhobitwidth (array): number of bits allocated to each phononic mode.
         wordsize (int): The wordsize that basis_states is given in. Default: 32.
     """
-    # XXX this way of creating new instances all the time seems might be improved upon
     _GenPhononAtExc(basis_states, posbitwidth, qhobitwidth, wordsize).add()
 
 
@@ -94,7 +93,7 @@ def calculate_bath_n_b(weights, basis_states, posbitwidth, qhobitwidth, wordsize
 ####################################################################################################
 # CUDA code snippets:
 
-_gpu_add32_prefix = r'''
+_gpu_add32 = r'''
     extern "C" __global__
     void add_pte_kernel(unsigned int* bs, 
         const unsigned int* bit_offset, const unsigned int* word_i, const unsigned int* num2bits,
@@ -102,26 +101,11 @@ _gpu_add32_prefix = r'''
     {
         unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
         if (tid < rownum) {
-            unsigned int pos    = bs[tid*rowlen] >> (32u-bit_offset[0]);
-            unsigned int arrind = tid*rowlen + word_i[pos];
-    '''
+            INDEXTYPE arrind  = tid*rowlen;
 
-_gpu_add32_core_vanilla = r'''
-            if (num2bits[pos] < 33) {
-                bs[arrind] += (1u << 32u-num2bits[pos]);
-            }
-            else {
-                bs[arrind+1]    += (1u << (64u-num2bits[pos]));
-                if ( bs[arrind+1]   >> (64u-num2bits[pos]) == ((~0u) >> (64u-num2bits)) ) {
-                    bs[arrind]      += 1;
-                }
-            }
-        }
-    }
-    '''
+            unsigned int pos    = bs[arrind] >> (32u-bit_offset[0]);
+            arrind              += word_i[pos];
 
-
-_gpu_add32_core = r'''
             // add an appropriately bit-shifted 1 to the first or second word:
             bs[arrind + (num2bits[pos] > 32u)] += 1u << ((num2bits[pos] > 32u)*32u + 32u - num2bits[pos]);
 
@@ -144,8 +128,10 @@ _gpu_rem32 = r'''
     {
         unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
         if (tid < rownum) {
-            unsigned int pos    = bs[tid*rowlen] >> (32u-bit_offset[0]);
-            unsigned int arrind = tid*rowlen + word_i[pos];
+            INDEXTYPE arrind  = tid*rowlen;
+
+            unsigned int pos    = bs[arrind] >> (32u-bit_offset[0]);
+            arrind              += word_i[pos];
 
             // if the subtraction is from the second word and would cause and underflow there,
             // then subtract 1 from the first word and bit-flip zeros in second word:
@@ -173,8 +159,10 @@ _gpu_get32 = r'''
     {
         unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
         if (tid < rownum) {
-            unsigned int pos    = bs[tid*rowlen] >> (32u-bit_offset[0]);
-            unsigned int arrind = tid*rowlen + word_i[pos];
+            INDEXTYPE arrind  = tid*rowlen;
+
+            unsigned int pos    = bs[arrind] >> (32u-bit_offset[0]);
+            arrind              += word_i[pos];
 
             result[tid]     = bs[arrind];
             result[tid]     &= ~0u >> (bit_offset[pos] - 32u*word_i[pos]);
@@ -201,7 +189,7 @@ _gpu_sum32 = r'''
     {
         unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
         if (tid < rownum) {
-            unsigned int arrind;
+            INDEXTYPE arrind;
             unsigned int tmp;
 
             for (unsigned int site = 0u; site < numsites; ++site) {
@@ -235,7 +223,7 @@ _gpu_calc_n_b32 = r'''
         unsigned int tid    = blockDim.x * blockIdx.x + threadIdx.x;
         int num_valid       = min(rownum - blockDim.x * blockIdx.x, blockDim.x);
         double thread_data;
-        unsigned int arrind;
+        INDEXTYPE arrind;
         unsigned int tmp;
 
         // Specialize BlockReduce
@@ -269,15 +257,46 @@ _gpu_calc_n_b32 = r'''
 ####################################################################################################
 # kernel definitions:
 
-_add_pte_kernel         = cupy.RawKernel(_gpu_add32_prefix + _gpu_add32_core, "add_pte_kernel")
+_add_pte_kernel     = cupy.RawKernel(_gpu_add32.replace("INDEXTYPE", "unsigned int"),
+                                                "add_pte_kernel")
 
-_remove_pte_kernel      = cupy.RawKernel(_gpu_rem32, "rem_pte_kernel")
+_remove_pte_kernel  = cupy.RawKernel(_gpu_rem32.replace("INDEXTYPE", "unsigned int"),
+                                                "rem_pte_kernel")
 
-_get_occ_pte_kernel     = cupy.RawKernel(_gpu_get32, "get_occ_pte_kernel")
+_get_occ_pte_kernel = cupy.RawKernel(_gpu_get32.replace("INDEXTYPE", "unsigned int"),
+                                                "get_occ_pte_kernel")
 
-_sum_occ_pte_kernel     = cupy.RawKernel(_gpu_sum32, "sum_occ_pte_kernel")
+_sum_occ_pte_kernel = cupy.RawKernel(_gpu_sum32.replace("INDEXTYPE", "unsigned int"),
+                                                "sum_occ_pte_kernel")
 
-_calc_bath_n_b_kernel   = cupy.RawKernel(_gpu_calc_n_b32, "calc_bath_n_b_kernel", backend="nvcc")
+_calc_bath_n_b_kernel = cupy.RawKernel(_gpu_calc_n_b32.replace("INDEXTYPE", "unsigned int"),
+                                                "calc_bath_n_b_kernel", backend="nvcc")
+
+
+_add_pte_kernel_64      = cupy.RawKernel(_gpu_add32.replace(
+                                            "INDEXTYPE", "unsigned long long").replace(
+                                            "add_pte_kernel", "add_pte_kernel_64"),
+                                        "add_pte_kernel_64")
+
+_remove_pte_kernel_64   = cupy.RawKernel(_gpu_rem32.replace(
+                                            "INDEXTYPE", "unsigned long long").replace(
+                                            "rem_pte_kernel", "rem_pte_kernel_64"),
+                                        "rem_pte_kernel_64")
+
+_get_occ_pte_kernel_64  = cupy.RawKernel(_gpu_get32.replace(
+                                            "INDEXTYPE", "unsigned long long").replace(
+                                            "get_occ_pte_kernel", "get_occ_pte_kernel_64"),
+                                        "get_occ_pte_kernel_64")
+
+_sum_occ_pte_kernel_64  = cupy.RawKernel(_gpu_sum32.replace(
+                                            "INDEXTYPE", "unsigned long long").replace(
+                                            "sum_occ_pte_kernel", "sum_occ_pte_kernel_64"),
+                                        "sum_occ_pte_kernel_64")
+
+_calc_bath_n_b_kernel_64 = cupy.RawKernel(_gpu_calc_n_b32.replace(
+                                            "INDEXTYPE", "unsigned long long").replace(
+                                            "calc_bath_n_b_kernel", "calc_bath_n_b_kernel_64"),
+                                            "calc_bath_n_b_kernel_64", backend="nvcc")
 
 ####################################################################################################
 # the central class that ties together all of the functions provided by this script:
@@ -309,12 +328,16 @@ class _GenDatSegBase:
                                 *[cupy.uint32(i) for i in basis_states.shape]]]
         self.wordsize       = wordsize
         self.numsites       = len(qhobitwidth)
+        if numpy.prod(basis_states.shape) > 4294967296:
+            self.suffix = "_64"
+        else:
+            self.suffix = ""
 
 class _GenPhononAtExc(_GenDatSegBase):
     def add(self):
         """See global function add_phonon_at_exc."""
         if self.wordsize == 32:
-            _add_pte_kernel(*self.funcargs)
+            globals()["_add_pte_kernel" + self.suffix](*self.funcargs)
         else:
             raise NotImplementedError("wordsizes other than 32 bits have not yet been implemented.")
 
@@ -322,7 +345,7 @@ class _GenPhononAtExc(_GenDatSegBase):
     def subtract(self):
         """See global function rem_phonon_at_exc."""
         if self.wordsize == 32:
-            _remove_pte_kernel(*self.funcargs)
+            globals()["_remove_pte_kernel" + self.suffix](*self.funcargs)
         else:
             raise NotImplementedError("wordsizes other than 32 bits have not yet been implemented.")
 
@@ -332,7 +355,7 @@ class _GenPhononAtExc(_GenDatSegBase):
         result          = cupy.zeros(int(self.funcargs[2][-2]), dtype=f"uint{self.wordsize}")
         self.funcargs[2] += [result,]
         if self.wordsize == 32:
-            _get_occ_pte_kernel(*self.funcargs)
+            globals()["_get_occ_pte_kernel" + self.suffix](*self.funcargs)
         else:
             raise NotImplementedError("wordsizes other than 32 bits have not yet been implemented.")
         # return the last item (i.e., results) and remove it from the list:
@@ -347,7 +370,7 @@ class _GenPhononAtExc(_GenDatSegBase):
         self.funcargs[2] += [result, omega_arr, cupy.uint32(self.numsites)]
 
         if self.wordsize == 32:
-            _sum_occ_pte_kernel(*self.funcargs)
+            globals()["_sum_occ_pte_kernel" + self.suffix](*self.funcargs)
         else:
             raise NotImplementedError("wordsizes other than 32 bits have not yet been implemented.")
         self.funcargs[2].pop()          # remove numsites from funcargs
@@ -364,7 +387,7 @@ class _GenAllPhonon(_GenDatSegBase):
         self.funcargs[2] += [result, weights, cupy.uint32(self.numsites)]
 
         if self.wordsize == 32:
-            _calc_bath_n_b_kernel(*self.funcargs)
+            globals()["_calc_bath_n_b_kernel" + self.suffix](*self.funcargs)
         else:
             raise NotImplementedError("wordsizes other than 32 bits have not yet been implemented.")
         self.funcargs[2].pop()          # remove numsites from funcargs
